9da5dd240e929c969d768836f128324f141c93e6
Bogdan Konovchenko <konovchenkobv@gmail.com>
2021-06-17 15:20:53 +0300

    Статусы заказов

--- src/orders/orders.model.ts
+++ src/orders/orders.model.ts
@@ -2,7 +2,15 @@ import { prop } from '@typegoose/typegoose';
 import { Base, TimeStamps } from '@typegoose/typegoose/lib/defaultClasses';
 
 export enum OrderStatus {
-  CREATED= 'created'
+  CREATED = 'created',
+  FINISHED = 'finished',
+  WAITING_INCOMING = 'waiting_incoming',
+  PARTLY_INCOMING = 'partly_incoming',
+  WORKING = 'working',
+  DRAFT = 'draft',
+  CANCEL = 'cancel',
+  PARTLY_CANCEL = 'partly_cancel',
+  REDEMPTION = 'redemption'
 }
 
 class Product extends Base {
