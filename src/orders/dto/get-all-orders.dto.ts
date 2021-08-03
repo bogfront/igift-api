5530855b13af94789ee1db4e6eea812e98aaf486
Bogdan Konovchenko <konovchenkobv@gmail.com>
2021-08-03 15:42:35 +0300

    ошибка сервиса

---
+++ src/orders/dto/get-all-orders.dto.ts
@@ -0,0 +1,3 @@
+export class GetAllOrdersDto {
+	user: any;
+}
--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -11,18 +11,12 @@ import {
   Request
 } from '@nestjs/common';
 import { OrdersModel } from './orders.model';
-import { OrderStatus } from "./orders.constants";
+import { OrderStatus } from './orders.constants';
 import { FindOrderDto } from './dto/find-order.dto';
 import { OrdersService } from './orders.service';
-import { JwtAuthGuard } from "../auth/guards/jwt.guard";
-import { ProductMetadataDto } from "./dto/product-metadata.dto";
-import {CreateOrderDto} from "./dto/create-order.dto";
-
-const metascraper = require('metascraper')([
-  require('metascraper-title')(),
-  require('metascraper-image')()
-])
-const got = require('got')
+import { JwtAuthGuard } from '../auth/guards/jwt.guard';
+import {CreateOrderDto} from './dto/create-order.dto';
+import {GetAllOrdersDto} from './dto/get-all-orders.dto';
 
 @Controller('orders')
 export class OrdersController {
@@ -31,39 +25,39 @@ export class OrdersController {
   @HttpCode(200)
   @UseGuards(JwtAuthGuard)
   @Post()
-  async getAll(@Request() req: any, @Body() dto: FindOrderDto) {
-    const user: string = req.user;
-    
-    return this.ordersService.find({
-      ...dto,
-      user
-    });
+  async getAll(@Request() req: GetAllOrdersDto, @Body() dto: FindOrderDto) {
+	const user: string = req.user;
+	
+	return this.ordersService.find({
+		...dto,
+		user
+	});
   }
 
   @UseGuards(JwtAuthGuard)
   @Post('create')
   async create(@Request() req: any, @Body() dto: CreateOrderDto) {
-    const user: string = req.user;
-    const status = OrderStatus.CREATED;
-    const number = await this.ordersService.getCount();
-    
-    return this.ordersService.create({
-      ...dto,
-      user,
-      status,
-      number
-    });
+	const user: string = req.user;
+	const status = OrderStatus.CREATED;
+	const number = await this.ordersService.getCount();
+	
+	return this.ordersService.create({
+		...dto,
+		user,
+		status,
+		number
+	});
   }
   
   @UseGuards(JwtAuthGuard)
   @Get(':id')
   async get(@Param('id') id: string) {
-    return this.ordersService.findById(id);
+	return this.ordersService.findById(id);
   }
 
   @Delete(':id')
   async delete(@Param('id') id: string) {
-    return this.ordersService.deleteById(id);
+	return this.ordersService.deleteById(id);
   }
 
   @Patch(':id')
@@ -72,12 +66,4 @@ export class OrdersController {
   @HttpCode(200)
   @Post()
   async find(@Body() dto: FindOrderDto) {}
-  
-  @Post('metadata')
-  async getMetadata(@Body() dto: ProductMetadataDto) {
-    const { body: html, url } = await got(dto.url);
-    const metadata = await metascraper({ html, url });
-    
-    return metadata;
-  }
 }
