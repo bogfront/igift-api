aa274a44c1288f47d924cbb5c7934529546f08e2
Bogdan Konovchenko <konovchenkobv@gmail.com>
2021-08-03 16:34:45 +0300

    ошибка сервиса

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -10,48 +10,50 @@ import { JwtService } from '@nestjs/jwt';
 @Injectable()
 export class AuthService {
   constructor(
-	@InjectModel(UserModel) private readonly userModel: ModelType<UserModel>,
-	private readonly jwtService: JwtService
+  	@InjectModel(UserModel) private readonly userModel: ModelType<UserModel>,
+		private readonly jwtService: JwtService
   ) {}
 
   async createUser(dto: RegisterDto) {
-	const salt = await genSalt(10);
-	const newUser = new this.userModel({
-		email: dto.email,
-		phone: dto.phone,
-		name: dto.name,
-		secondName: dto.secondName,
-		passwordHash: await hash(dto.password, salt),
-	});
-	
-	
-	await newUser.save();
-	return this.login(dto.email);
+		const salt = await genSalt(10);
+		
+		const newUser = new this.userModel({
+			email: dto.email,
+			phone: dto.phone,
+			name: dto.name,
+			secondName: dto.secondName,
+			passwordHash: await hash(dto.password, salt),
+		});
+		
+		await newUser.save();
+		
+		return this.login(dto.email);
   }
 
   async findUser(email: string) {
-	return this.userModel.findOne({ email }).exec();
+		return this.userModel.findOne({ email }).exec();
   }
 
-  async validateUser(
-	email: string,
-	password: string,
-  ): Promise<Pick<UserModel, 'email'>> {
-	const user = await this.findUser(email);
-	if (!user) {
-		throw new UnauthorizedException(USER_NOT_FOUND_ERROR);
-	}
-	const isCorrectPassword = await compare(password, user.passwordHash);
-	if (!isCorrectPassword) {
-		throw new UnauthorizedException(WRONG_PASSWORD_ERROR);
-	}
-	return { email: user.email };
+  async validateUser(email: string, password: string): Promise<Pick<UserModel, 'email'>> {
+		const user = await this.findUser(email);
+		
+		if (!user) {
+			throw new UnauthorizedException(USER_NOT_FOUND_ERROR);
+		}
+		
+		const isCorrectPassword = await compare(password, user.passwordHash);
+		
+		if (!isCorrectPassword) {
+			throw new UnauthorizedException(WRONG_PASSWORD_ERROR);
+		}
+		
+		return { email: user.email };
   }
 
   async login(email: string) {
-	const payload = { email };
-	return {
-		access_token: await this.jwtService.signAsync(payload),
-	};
+		const payload = { email };
+		return {
+			access_token: await this.jwtService.signAsync(payload),
+		};
   }
 }
